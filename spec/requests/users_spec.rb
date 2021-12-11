aa1411813178d0be19a9a87ed8814553ef159ee4
kossi-stack <kossifioklou10@gmail.com>
2021-12-11 12:01:50 -0800

    Create renders correct test

--- spec/requests/users_spec.rb
+++ spec/requests/users_spec.rb
@@ -6,4 +6,12 @@ RSpec.describe "Users", type: :request do
     it 'check status' do
       expect(response).to have_http_status(:ok)
   end
+
+  it 'renders correct template' do
+    expect(response).to render_template('index')
+  end
+
+  it 'renders correct template' do
+    expect(response).to_not render_template('show')
+  end
 end
