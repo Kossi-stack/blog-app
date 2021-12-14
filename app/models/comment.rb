392e2e3bcf0ba0fb992062931cda2776eecb76ae
kossi-stack <kossifioklou10@gmail.com>
2021-12-13 23:28:39 -0800

    Add data types for comments

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,8 @@
 class Comment < ApplicationRecord
+  belongs_to :author, foreign_key: 'User'
+  belongs_to :post, foreign_key: 'post'
+
+  def update_comments_counter
+    post.increment!(:comments_counter)
+  end
 end
