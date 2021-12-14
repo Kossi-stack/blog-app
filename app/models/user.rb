8c5845a7ba4afa9d514f013298aa525bf9edb02b
kossi-stack <kossifioklou10@gmail.com>
2021-12-13 23:16:12 -0800

    Add data types for posts

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,9 @@
 class User < ApplicationRecord
+    has_many :likes, foreign: 'author_id'
+    has_many :posts, foreign: 'author_id'
+    has_many :comments, foreign: 'author_id'
+
+    def recents_posts
+      posts.limit(3).order(created_at: :desc)
+    end
 end
