a8377323cb78d173cbb9e3af83af23996942cbae
kossi-stack <kossifioklou10@gmail.com>
2021-12-15 10:31:38 -0800

    fix linters

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,7 +1,7 @@
 class Comment < ApplicationRecord
   belongs_to :author, class_name: 'User'
   belongs_to :post, foreign_key: 'post'
-  
+
   private
   def update_comments_counter
     post.increment!(:comments_counter)
--- app/models/like.rb
+++ app/models/like.rb
@@ -3,6 +3,7 @@ class Like < ApplicationRecord
   belongs_to :post, foreign_key: 'post_id'
 
   private
+
   def update_likes_counter
     post.increment!(:likes_counter)
   end
