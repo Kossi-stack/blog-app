97ff15013de00b38a7624739fbffbe9f38b728e5
kossi-stack <kossifioklou10@gmail.com>
2021-12-15 10:20:29 -0800

    Add update post counter to private

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,12 +2,13 @@ class Post < ApplicationRecord
   belongs_to :author, class_name: 'User'
   has_many :comments, foreign_key: 'post_id'
   has_many :likes, foreign_key: 'post_id'
-
-  def update_post_counter
-    user.increment!(:posts_counter)
-  end
-
+ 
   def recent_comments
     comments.limt(5).order(created_at: :desc)
+  end
+
+  private
+  def update_post_counter
+    user.increment!(:posts_counter)
   end
 end
