8f66c16f24888eff162e2a5c9229fa7dfa04317b
kossi-stack <kossifioklou10@gmail.com>
2021-12-18 19:46:41 -0800

    Declare a path for new.html

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,9 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root'users#index'
 
-  resources :users, only: [:index, :show] do
-    resources :posts, only: [:index, :show]
+  resources :users, only: [:index, :show, :new, :create] do
+    resources :posts, only: [:index, :show, :new, :create] do
+      resources :comments, only: [:new, :create]
+    end
   end
 end
