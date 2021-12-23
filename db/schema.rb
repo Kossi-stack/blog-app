dad3606a0ef32309ad1ab7d750452849edfbf310
kossi-stack <kossifioklou10@gmail.com>
2021-12-23 11:46:14 -0800

    Install and set-up devise

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2021_12_14_204638) do
+ActiveRecord::Schema.define(version: 2021_12_22_212825) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -50,8 +50,21 @@ ActiveRecord::Schema.define(version: 2021_12_14_204638) do
     t.string "photo"
     t.text "bio"
     t.integer "post_counter"
-    t.datetime "created_at", precision: 6, null: false
-    t.datetime "updated_at", precision: 6, null: false
+    t.string "email"
+    t.string "encrypted_password"
+    t.string "reset_password_token"
+    t.string "unconfirmed_email"
+    t.string "confirmation_token"
+    t.string "surname"
+    t.datetime "reset_password_sent_at"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "remember_created_at"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "comments", "posts"
